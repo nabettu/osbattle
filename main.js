ff6ebc8970a9609a1014a4b65aa54d49c646fbe6
tatsuakiw <tatsuaki.w@gmail.com>
2015-05-15 14:13:36 +0900

    push監視してグラフへ反映

--- main.js
+++ main.js
@@ -3,22 +3,46 @@ var osDataStore = milkcocoa.dataStore("userData");
 
 var userData = {
   data: [],
+  osResult: {apple:0,android:0,windows:0,others:0},
   init: function(){
-    //ユーザー一覧を読み込むにUserListに突っ込む
+    //ユーザー一覧を読み込みdataに突っ込む
     osDataStore.stream().size(999).next(function(err,data){
-      console.log(data);
+//      console.log(data);
       var cuttentUserExist = true;
       for(var i=0; i<data.length; i++){
-        userData.data.push(data[i].value);
+        userData.addUser(data[i].value);
       //userDataに今みてるユーザーが入ってないかチェックする
         if(data[i].value.ipAddress = ipAddress)cuttentUserExist = false;
       }
       if(cuttentUserExist)userData.addCurrentUser();
-      else console.log("既にこのユーザー情報は送信してます");
+      else userData.graphLoad();
     });
   },
   graphLoad: function(){
-
+    var doughnutData = [
+    　　{
+      //apple
+    　　　value: userData.osResult.apple,
+    　　　color:"#999999"
+    　　},
+    　　{
+      //android
+    　　　value: userData.osResult.android,
+    　　　color: "#a5c63b"
+    　　},
+    　　{
+      //windows
+    　　　value: userData.osResult.windows,
+    　　　color: "#00adef"
+    　　},
+       {
+      //others
+    　　　value: userData.osResult.others,
+    　　　color: "#fbaa6e"
+    　　}
+    ];
+ 
+    var myDoughnut = new Chart($("#chart")[0].getContext("2d")).Doughnut(doughnutData);
   },
   addCurrentUser: function(){
     //milkcocoaへデータ送信
@@ -31,11 +55,22 @@ var userData = {
         console.log("milkcocoa送信完了!");
       }
     );
+  },
+  addUser: function(addData){
+    userData.data.push(addData);
+    userData.osResult[addData.os]++;
   }
 }
 
 $(function(){
   userData.init();
+
+  //データ受信監視
+  osDataStore.on("push",function(data){
+    userData.addUser(data.value);
+    setTimeOut("userData.graphLoad()",1000);
+  });
+
 });
 
 function findOS(ua){
